5a8dac652d7a9a7e1c93b01047369901ff3e9c0f
Joseffus Santos <joseffus.santos@gmail.com>
2017-10-16 23:23:03 -0500

    init2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>
+#include <WiFiClient.h>
+#include "TimeLib.h"
+//#include "NtpClientLib.h"
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include <ESP8266mDNS.h>
+#include <Ticker.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+#include "FSWebServerLib.h"
+#include <Hash.h>
+
+
+
 
 void setup() {
-    // put your setup code here, to run once:
+    // WiFi is started inside library
+    SPIFFS.begin(); // Not really needed, checked inside library and started if needed
+    ESPHTTPServer.begin(&SPIFFS);
+
+    /* add setup code here */
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    /* add main program code here */
+
+    // DO NOT REMOVE. Attend OTA update from Arduino IDE
+    //ESPHTTPServer.handle();
 }
